66323cf41d0d99cb10cac93b6843f5e129d58867
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-03-19 22:15:51 +0600

    added h1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [count, setCount] = useState(0)
   return (
     <div>
+      <h1 className='text-center my-2'>My super duper fake store !!</h1>
       <Navbar count={count}></Navbar>
       <AllProducts setCount={setCount} count={count}></AllProducts>
     </div>
